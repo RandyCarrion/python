76c0757134926f45f730ade18fbc051c24049c48
Randy Carrion <randycarrion@randys-mbp.ams.nl.ibm.com>
2018-08-21 15:25:00 +0200

    bank.py added

--- bank.py
+++ bank.py
@@ -5,14 +5,21 @@ class Account:
         self.min_balance = min_balance
         
         
-    def deposit (self, amount):
+    def deposit(self, amount):
         self.balance += amount
         
-    def withdrawal (self, amount):
+    def withdraw(self, amount):
         if self.balance - amount >= self.min_balance:
             self.balance -= amount
         else:
             print("Sorry, not enough funds!")
             
     def statement (self):
-        print("Account Balance: ${}".format(self.balance))
+        print("Account Balance: €{}".format(self.balance))
+        
+class Current(Account): 
+    def __init__(self, name, balance):
+        super().__init__(name, balance, min_balance = -10)
+        
+    def __str__(self):
+        return "{}'s Current Account : Balance €{}".format(self.name, self.balance)
